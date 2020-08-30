92f873c8574d7d0268ede724d44aa0b31dbc14f0
AhmmedAhsan3307 <tanzim.ahsan2014@gmail.com>
2020-08-31 09:24:47 +1000

    ye

--- src/StageReader.java
+++ src/StageReader.java
@@ -3,9 +3,9 @@ import java.util.*;
 import java.util.regex.*;
 
 class StageReader {
-    public static Stage readStage(String path){
+    public static Stage readStage(String path) {
         Stage stage = new Stage();
-        try{
+        try {
             Properties props = (new Properties());
             props.load(new FileInputStream(path));
             for (String key : props.stringPropertyNames()) {
@@ -16,24 +16,26 @@ class StageReader {
                 if (cellMatcher.matches()) {
                     stage.grid.cellAtColRow(cellMatcher.group(1).charAt(0), Integer.parseInt(cellMatcher.group(2)))
                             .ifPresent(cellsInQuestion::add);
-                } else {System.out.println("no match");}
+                } else {
+                    System.out.println("no match");
+                }
                 for (Cell c : cellsInQuestion) {
-                    if (value.equals("puppy red")){
+                    if (value.equals("puppy red")) {
                         stage.actors.add(new Puppy(c, 1.0f));
-                    }else if (value.equals("puppy blue")){
+                    } else if (value.equals("puppy blue")) {
                         stage.actors.add(new Puppy(c, 0.0f));
-                    }else if (value.equals("lion red")){
+                    } else if (value.equals("lion red")) {
                         stage.actors.add(new Lion(c, 1.0f));
-                    }else if (value.equals("lion blue")){
+                    } else if (value.equals("lion blue")) {
                         stage.actors.add(new Lion(c, 0.0f));
-                    }else if (value.equals("bunny red")){
+                    } else if (value.equals("bunny red")) {
                         stage.actors.add(new Rabbit(c, 1.0f));
-                    }else if (value.equals("bunny red")){
+                    } else if (value.equals("bunny red")) {
                         stage.actors.add(new Rabbit(c, 0.0f));
                     }
                 }
             }
-        } catch (IOException e){
+        } catch (IOException e) {
 
         }
         return stage;
